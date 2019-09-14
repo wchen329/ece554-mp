02a0136d413a7571b7bb154e58ee0a1848220646
ychen664 <38704497+ychen664@users.noreply.github.com>
2019-09-14 16:52:55 -0500

    Corrected file

--- Lab1-SPART/spart.v
+++ Lab1-SPART/spart.v
@@ -30,5 +30,48 @@ module spart(
     input rxd
     );
 
+	wire data;
+	wire sample_en, bit_en;
+	
+	parameter IDLE = 2'b00, ACTIVE = 2'b01;
+	reg rx_state;
+	reg [15:0] rx_state_count;
+	reg rx_nxt_state;
+	
+	assign databus = (iocs & ~iorw) ? data : 8'bZ;
+	
+	always @* begin
+		if (ioaddr == 2'b01 && iorw == 1'b1)
+			data = // TODO
+			
+	
+	end
+	
+	always@(posedge clk) begin
+		if (rst)
+			rx_state <= IDLE; 
+		else
+			rx_state <= rx_nxt_state;
+	
+	always@(posedge clk)
+		if (sample_en) begin
+			case(rx_state)
+				IDLE:
+					if (rxd == 1'b0) begin
+						rx_state_count <= rx_state_count + 1;
+					end
+					if (rx_state_count > 16'd8)
+						rx_nxt_state <= ACTIVE;
+					else
+						rx_nxt_state <= IDLE;
+				ACTIVE:
+					if (rxd == 1'b0) begin
+						
+					end
+			endcase
+		end
+	
+	//Counter
+	
 
 endmodule
