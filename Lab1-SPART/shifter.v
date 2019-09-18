a850cb171e6417d66d0097ed66a2d49018ab6aa0
ychen664 <38704497+ychen664@users.noreply.github.com>
2019-09-18 18:09:48 -0500

    New shifter with rxd as input

--- Lab1-SPART/shifter.v
+++ Lab1-SPART/shifter.v
@@ -1,7 +1,8 @@
-module shifter(input nxt_bit, input clk, input rst, input en, output [7:0]data, output has_data);
+module shifter(input rxd, input clk, input rst, input baud_en, output [7:0]data, output has_data);
 	reg [7:0] shifter_data;
 	reg [7:0] buffer;
 	reg [3:0] count;
+	reg [3:0] bit_count;
 	reg full;
 	
 	assign data = buffer;
@@ -13,15 +14,42 @@ module shifter(input nxt_bit, input clk, input rst, input en, output [7:0]data, 
 			buffer <= 8'b0;
 			full <= 1'b0;
 			count <= 4'b0;
+			bit_count <= 4'b0;
 		end
-		else if (en) begin
-			if (count < 4'd8 && full == 1'b0) begin
-				count <= count + 4'd1;
-				shifter_data <= {shifter_data[6:0], nxt_bit};
-			end
-			else begin
-				buffer <= shifter_data;
-				full <= 1'b1;
+		else if (baud_en) begin
+			if (full == 1'b0) begin
+				if (count < 4'd8 && bit_count == 4'b0) begin
+					if (rxd == 1'b0) begin
+						count <= count + 4'd1;
+					end
+					else begin
+						count <= (count == 4'b0) ? 4'b0 : (count - 4'd1);
+					end
+				end
+				else if (bit_count == 4'b0) begin
+					bit_count <= bit_count + 4'd1;
+					count <= 4'd0;
+				end
+				else if (bit_count > 4'b0 && count < 4'd15) begin
+					count <= count + 4'd1;
+				end
+				else if (bit_count > 4'b0 && bit_count < 4'd9 && count == 4'd15) begin
+					count <= 4'b0;
+					shifter_data <= {rxd, shifter_data[7:1]};
+					bit_count <= bit_count + 4'd1;
+				end
+				else if (bit_count == 4'd9 && count == 4'd15) begin
+					if (rxd == 1'b1) begin
+						buffer <= shifter_data;
+						full <= 1'b1;
+						count <= 4'b0;
+						bit_count <= 4'b0;
+						shifter_data <= 8'b0;
+					end
+				end
+				else begin
+					$display("Getting into Nowhere!");
+				end
 			end
 		end
 	end
--- Lab1-SPART/shifter_tb.v
+++ Lab1-SPART/shifter_tb.v
@@ -2,38 +2,46 @@ module shifter_tb();
 
 reg clk;
 reg rst;
-reg nxt_bit;
-reg en;
+reg rxd;
+reg baud_en;
 reg [7:0] collected;
+reg [19:0] test_data;
 
 wire [7:0] data;
 wire has_data;
 
 initial begin
 clk = 0;
+rxd = 1;
 rst = 1;
-nxt_bit = 0;
-en = 0;
+baud_en = 0;
 collected = 8'b0;
+test_data = 20'b1101010100_1010101010;
 end
 
 always #5 clk = ~clk;
 
-always #10 nxt_bit = $random;
+always #480 begin
+	rxd = test_data[0];
+	test_data = {test_data[0], test_data[19:1]};
+end
 
-always #30 en = ~en;
+always #20 begin 
+	baud_en = ~baud_en;
+	#10
+	baud_en = ~baud_en;
+end
 
 always @(posedge clk) begin
-	if (has_data) begin
+	if (has_data && ~rst) begin
 		collected <= data;
 		rst <= 1;
-		en <= 0;
 	end
 	else begin
 		rst <= 0;
 	end
 end
 
-shifter s1( nxt_bit,  clk,  rst,  en,  data,  has_data);
+shifter s1(rxd, clk, rst, baud_en, data, has_data);
 
 endmodule
