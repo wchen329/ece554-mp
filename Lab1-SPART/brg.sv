c8ddd29187a4396ac6c2fe37398c3404b44dbac2
Winor Chen <wchen329@wisc.edu>
2019-09-17 19:12:58 -0500

    make it so that set is done in two separate cycles for BRG.

--- Lab1-SPART/brg.sv
+++ Lab1-SPART/brg.sv
@@ -1,10 +1,18 @@
-module BRG(input clk, start, input[31:0] bbase, output enable);
+/* BRG
+ * a.k.a. Baud Rate Generator
+ *
+ * clk - clock signal
+ * start - raise this to begin a new baud rate count. Raising this also enables div_in for writing
+ * div_hword - if HIGH then div_in is set to the high word of the divisor buffer
+ *             if LOW then div_in is set to the low word of the divisor buffer
+ */
+module BRG(input clk, start, div_hword, input[7:0] div_in, output enable);
 
 	localparam HZ_PER_TERAHERTZ = 64'd1000000000000;
 
 	// Frequency Counter
 	reg[31:0] down_counter;
-	reg[31:0] br;
+	reg[16:0] br; // divisor buffer
 
 	// Phase Setting
 	always @(posedge clk) begin
@@ -16,7 +24,12 @@ module BRG(input clk, start, input[31:0] bbase, output enable);
 			///////////////////
 			// BASE REGISTER //
 			///////////////////
-			br = bbase;
+			if(div_hword) begin
+				br[7:0] = div_in;
+			end
+			else begin
+				br[15:8] = div_in;
+			end
 		end
 
 		// Otherwise, go set the down counter register and the base register
